3ca8a8baaa06e03ee168c71b50030f92d61bc969
chenfanda <fanda.hk2013@gmail.com>
2026-03-30 10:52:49 +0800

    test fe:add NEXT_PUBLIC_API_BASE

--- todo-fe/next.config.js
+++ todo-fe/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const apiBase = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:3000"
+
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3000/:path*",
+        destination: `${apiBase}/:path*`,
       },
     ]
   },
